7f4a4fb4ff2b4751a002b21984fd98ee7b0ee456
unknown <ferindyaulia19@gmail.com>
2023-04-30 15:15:23 +0700

    add kmp & bm algorithm

---
+++ src/BMAlgorithm.js
@@ -0,0 +1,42 @@
+const buildLast = (pattern) => {
+    const last = new Array(128)
+
+    for (let i = 0; i < pattern.length; i++) {
+        last[pattern.charAt(i)] = i
+    }
+    return last
+}
+
+const bmMatch = (text, pattern) => {
+    const n = text.length
+    const m = pattern.length
+    const last = buildLast(pattern)
+
+    let i = m - 1
+    let j = m - 1 
+    
+    if (i > n - 1) {
+        return -1
+    }
+    else {
+        do {
+            if (pattern.charAt(j) == text.charAt(i)) {
+                if (j == 0) {
+                    return i
+                }
+                else {
+                    i--
+                    j--
+                }
+            }
+            else {
+                const lo = last[text.charAt(i)]
+                i += m - Math.min(j, 1 + lo) 
+                j = m - 1
+            }
+        }
+        while (i <= n - 1)
+            return -1
+    }
+}
+
